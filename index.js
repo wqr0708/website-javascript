c1e20ddf90194657def04da6daba23ff0e660235
wqr0708 <60957105+wqr0708@users.noreply.github.com>
2025-01-14 00:24:38 -0500

    math methods

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const decreastBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increastBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+let x = 3.61;
+let y = 2;
+let z = 1;
 
-increastBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sin(x);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+let max = Math.max(x, y, z)
+let min = Math.min(x, y, z)
 
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
-
-decreastBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
+console.log(max);
