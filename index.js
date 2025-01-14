a1d8ffe15d84fa4b1d64d47fea2f33eeb1daa25d
wqr0708 <60957105+wqr0708@users.noreply.github.com>
2025-01-13 20:23:29 -0500

    change elements' value

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 // window.alert("This is an alert!");
 // window.alert("I like pizza!");
 
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza!"
+
 //This is a comment.
 
 /*
