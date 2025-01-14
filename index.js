e2c8ba2e988fde2bd9124581af24a38c30ecdbec
wqr0708 <60957105+wqr0708@users.noreply.github.com>
2025-01-13 20:17:03 -0500

    some basic grammer

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("Hello");
+// console.log("I like pizza!")
+
+// window.alert("This is an alert!");
+// window.alert("I like pizza!");
+
+//This is a comment.
+
+/*
+    This
+    is
+    a
+    comment
+*/
