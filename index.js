448ea727233de7f92a7815b6c13a949448104afb
wqr0708 <60957105+wqr0708@users.noreply.github.com>
2025-01-13 21:06:36 -0500

    update index.html and index.js to add new paragraph elements and display user information

--- index.js
+++ index.js
@@ -4,26 +4,10 @@
 // 1.decalaration   let x;
 // 2. initialization  x = 100;
 
-// let x;
-// x = 100;
+let fullName = "Bro Code";
+let age = 23;
+let student = true;
 
-// console.log(x);
-// console.log(y);
-
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
-
-let firstName = "Bro";
-
-let online = true;
-
-console.log(typeof age);
-console.log(`You are ${age} years old.`);
-console.log(`The price is $${price}`);
-console.log(`Your gpa is ${gpa}`);
-
-console.log(typeof firstName);
-console.log(`Your name is ${firstName}`);
-
-console.log(`Bro is online: ${online}`);
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
